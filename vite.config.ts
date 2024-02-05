149717bf5f4c9f5c7144dac5168bbf7f370e03d2
yazid69 <yazidlembirik69@outlook.fr>
2024-02-05 15:00:34 +0100

    test offline

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,35 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from "@vitejs/plugin-react"
+import { VitePWA } from 'vite-plugin-pwa'
+import { defineConfig } from "vite"
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react({
+      jsxRuntime: 'automatic',
+    }),
+
+    VitePWA({
+      registerType: 'autoUpdate',
+      injectRegister: 'auto',
+      devOptions: {
+        enabled: true
+      },
+      workbox: {
+        navigateFallback: 'index.html',
+        globPatterns: ['**/*.{js,css,html,ico,png,svg}'],
+      },
+      manifest: {
+        name: "Vite App",
+        short_name: "Vite App",
+        theme_color: "#ffffff",
+        icons: [
+          {
+            "src": "favicon.ico",
+            "sizes": "144x144",
+            "type": "image/x-icon"
+          },
+        ]
+      }
+    }),
+  ],
 })
