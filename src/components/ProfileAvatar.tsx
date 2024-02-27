5dd759288572bd6b83d4aae590b8d4df83fba404
yazid69 <yazidlembirik69@outlook.fr>
2024-02-27 10:16:47 +0100

    before deploiement

--- src/components/ProfileAvatar.tsx
+++ src/components/ProfileAvatar.tsx
@@ -1,23 +1,44 @@
-// import Avatar from '@mui/material/Avatar';
+import Avatar from '@mui/material/Avatar';
+import * as React from 'react';
+import Stack from '@mui/material/Stack';
+
+interface ProfileAvatarProps {
+    onClick?: (event: React.MouseEvent<HTMLElement>) => void;
+}
+
+const ProfileAvatar: React.FC<ProfileAvatarProps> = ({ onClick }) => {
+    return (
+        <Stack direction="row" spacing={2} onClick={onClick}>
+            <Avatar src="/broken-image.jpg" />
+
+        </Stack>
+    );
+
+};
+
+export default ProfileAvatar;
+
 // import * as React from 'react';
+// import Avatar from '@mui/material/Avatar';
 // import Stack from '@mui/material/Stack';
+// import { deepOrange } from '@mui/material/colors';
 
-// interface ProfileAvatarProps {
-//     src: string;
-//     name: string;
-//     onClick?: (event: React.MouseEvent<HTMLElement>) => void;
-// }
-
-// const ProfileAvatar: React.FC<ProfileAvatarProps> = ({ onClick }) => {
+// export default function FallbackAvatars() {
 //     return (
-//         <Stack direction="row" spacing={2} onClick={onClick}>
+//         <Stack direction="row" spacing={2}>
+//             <Avatar
+//                 sx={{ bgcolor: deepOrange[500] }}
+//                 alt="Remy Sharp"
+//                 src="/broken-image.jpg"
+//             >
+//                 B
+//             </Avatar>
+//             <Avatar
+//                 sx={{ bgcolor: deepOrange[500] }}
+//                 alt="Remy Sharp"
+//                 src="/broken-image.jpg"
+//             />
 //             <Avatar src="/broken-image.jpg" />
-
 //         </Stack>
-
 //     );
-
-// };
-
-// export default ProfileAvatar;
-
+// }
